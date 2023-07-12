cffb14de2610f63b2c4065fc548fb006b239ca71
Abrar Hussain <65803677+AbrarTunio@users.noreply.github.com>
2023-07-13 00:59:54 +0500

    Add files via upload
    few more changes for interactivity in all three files

--- script.js
+++ script.js
@@ -2,8 +2,45 @@ let imageContainer = document.querySelector("#image-container");
 let devName = document.querySelector(".name");
 let portfolio = document.querySelector("#portfolio");
 let hiddenAnchor = document.querySelector(".hidden-anchor");
+var lastScrollTop = 0;
 
-let list = [imageContainer, devName];
+function scrollFunction() {
+  let scrollDirection = document.documentElement.scrollTop;
+  //   A condition for image
+  if (scrollDirection > lastScrollTop) {
+    imageContainer.classList.add("image-scroll-behavior");
+    console.log(lastScrollTop);
+  } else {
+    imageContainer.classList.remove("image-scroll-behavior");
+    console.log("going back", lastScrollTop);
+  }
+  // A condition for cards
+  if (scrollDirection > lastScrollTop && scrollDirection > 120) {
+    document.querySelectorAll(".card").forEach((card) => {
+      card.classList.add("show-cards");
+    });
+  } else {
+    document.querySelectorAll(".programming-card").forEach((card) => {
+      card.classList.remove("show-cards");
+    });
+  }
+  lastScrollTop = scrollDirection <= 0 ? 0 : scrollDirection;
+}
+
+document.addEventListener("scroll", scrollFunction);
+
+// document.addEventListener(
+//   "scroll",
+//   () => {
+//     if (scrollDirection > lastScrollTop && scrollDirection >= 130) {
+//       console.log(scrollDirection, lastScrollTop);
+//     } else {
+//       console.log("small");
+//     }
+//     lastScrollTop = scrollDirection <= 0 ? 0 : scrollDirection;
+//   },
+//   false
+// );
 
 function apply() {
   if (portfolio.innerText != "ENGR. ABRAR HUSSAIN") {
